fa15163432e94447139de5917df546995a59eca0
Redmori <72279911+Redmori@users.noreply.github.com>
2024-04-15 02:25:09 +0200

    Added the 3 enemy types animated
    Ready for tower/demon animation

--- Scripts/enemy_resource.gd
+++ Scripts/enemy_resource.gd
@@ -3,7 +3,7 @@ extends Resource
 class_name Enemy
 
 @export var name : String
-@export var texture : CompressedTexture2D
+@export var texture : SpriteFrames
 @export var speed : float
 @export var health : int
 @export var worth : int
--- Scripts/walker.gd
+++ Scripts/walker.gd
@@ -5,15 +5,23 @@ var line : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var old_pos = position
 	progress += delta * speed
+	var direction = position - old_pos
+	if direction.x < 0 and abs(direction.x) > abs(direction.y):
+		$Enemy.get_node("Sprite2D").flip_h = true
+	elif direction.x > 0  and abs(direction.x) > abs(direction.y):
+		$Enemy.get_node("Sprite2D").flip_h = false
+	
 
 func set_stats(stats):
 	speed = stats.speed
-	$Enemy.get_node("Sprite2D").texture = stats.texture
+	$Enemy.get_node("Sprite2D").sprite_frames = stats.texture
+	$Enemy.get_node("Sprite2D").play()
 	$Enemy.set_max_hp(stats.health)
 	$Enemy.worth = stats.worth	
